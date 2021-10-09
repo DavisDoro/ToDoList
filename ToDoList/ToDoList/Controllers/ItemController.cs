defff36189acffe6692ac8956a26fd9e1b4eb1e2
DavisDoro <deivs65@gmail.com>
2021-10-09 17:59:38 +0300

    MVC skeleton implemented

---
+++ ToDoList/ToDoList/Controllers/ItemController.cs
@@ -0,0 +1,40 @@
+﻿using ToDoList.Data;
+using ToDoList.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ItemController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Item> objList = _db.Items;
+            return View(objList);
+        }
+
+        //GET-create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Post-Create method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Item obj)
+        {
+            _db.Items.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
