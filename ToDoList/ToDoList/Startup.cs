1e9ef3d44c83ae020519dd75c0a1454c0fa7c22b
Vitaly <vit@bb-tech.eu>
2021-10-20 21:31:01 +0300

    aaaa

--- ToDoList/ToDoList/Startup.cs
+++ ToDoList/ToDoList/Startup.cs
@@ -25,9 +25,19 @@ namespace ToDoList
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
-                );
+            if (string.IsNullOrEmpty(Configuration.GetConnectionString("DefaultConnection"))==true)
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                                     options.UseInMemoryDatabase("ta")
+                                                             );
+            }
+            else
+            {
+                services.AddDbContext<ApplicationDbContext>(options => 
+                                    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")) 
+                                                            );
+            }
+
             services.AddControllersWithViews();
         }
 
